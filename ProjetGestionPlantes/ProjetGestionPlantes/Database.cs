40b73f085e648567cd03b487afb584676d37deeb
Aliya <aliya.mz@eduge.ch>
2020-10-28 11:29:23 +0100

    Update (ajout BD)

---
+++ ProjetGestionPlantes/ProjetGestionPlantes/Database.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace ProjetGestionPlantes
+{    
+    public class Database
+    {
+        readonly SQLiteAsyncConnection _database;
+
+        public Database(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Plante>().Wait();
+        }
+
+        public Task<List<Plante>> GetPeopleAsync()
+        {
+            return _database.Table<Plante>().ToListAsync();
+        }
+
+        public Task<int> SavePersonAsync(Plante plante)
+        {
+            return _database.InsertAsync(plante);
+        }
+    }
+}
---
+++ ProjetGestionPlantes/ProjetGestionPlantes/Plante.cs
@@ -0,0 +1,17 @@
+﻿/*
+Classe Plante qui stockera les données relatives à chaque plante 
+ */
+
+using SQLite;
+
+namespace ProjetGestionPlantes
+{
+    public class Plante
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID_PLANTE { get; set; }
+        public string Nom { get; set; }
+        public string Notes { get; set; }
+        public int IdEspece { get; set; }
+    }
+}
